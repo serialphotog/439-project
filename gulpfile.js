7bf277c8c69ceb00aa531318e05d40e74139422d
Adam Thompson <adam@serialphotog.com>
2020-02-03 11:44:48 -0500

    Made build system compatible with a GitHub pages setup

--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,17 @@ var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
 var fancy_log = require('fancy-log');
 
-var paths = {
-	pages: ['src/html/**/*.html'],
-	styles: ['src/css/**/*.css'],
-	js: ['src/js/**/*.js'],
-};
-
 // The watch task
 var watched = watchify(browserify({
 	basedir: '.',
 	debug: true,
-	entries: './src/js/calculator.js',
+	entries: './src/calc.js',
 	cache: {},
 	packageCache: {}
 })).on('error', (err) => {
 	console.log(err.message);
 	this.emit('end');
 });
-
-// copies all of the html resources to the final distribution directory
-gulp.task('copy-html', () => {
-	return gulp.src(paths.pages)
-		   	.pipe(gulp.dest('dist'));
-});
-
-// Performs the full assets copy
-gulp.task('copy', gulp.series(gulp.parallel('copy-html'), () => {
-	return gulp.src(paths.styles)
-			.pipe(gulp.dest('dist/css'));
-}));
 
 // Bundles all of the Javascript resources
 function bundle() {
@@ -46,16 +28,15 @@ function bundle() {
 			.pipe(sourcemaps.init({loadMaps: true}))
 			.pipe(uglify())
 			.pipe(sourcemaps.write('./'))
-			.pipe(gulp.dest('dist/js'));
+			.pipe(gulp.dest('scripts/'));
 }
 
 // Watches for changes to watched files
 gulp.task('watch', () => {
-	gulp.watch(['src/css/**/*.css', 'src/html/**/*.html'], gulp.parallel('copy'));
 	gulp.watch(['src/js/**/*.js'], bundle);
 });
 
 // Setup the tasks
-gulp.task('default', gulp.series(gulp.parallel('copy'), bundle, gulp.parallel('watch')));
+gulp.task('default', gulp.series(bundle, gulp.parallel('watch')));
 watched.on('update', bundle);
 watched.on('log', fancy_log);
